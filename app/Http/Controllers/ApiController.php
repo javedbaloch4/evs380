6e1e260b0e4361b90d7e8c19b61e0a49b9b3f6e2
DESKTOP-SEHO8T9\Muhammad Saqib Tariq <mrsaqibtariq@gmail.com>
2018-09-07 22:25:26 +0500

    Api Structure : 07-09-2018

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+//use App\Http\Models\User;
 class ApiController extends Controller
 {
     public function buildApiResults(Request $request){
@@ -11,6 +11,60 @@ class ApiController extends Controller
         // User Model 
         
         // User Model => addToUser => return 
-        dd(json_decode($request->get('req')));
+        
+        $final_array = array();
+        if(empty($request->get('req'))){
+            $final_array['message'] = "req key not found in get / post array";
+            $final_array['status'] = 400;
+            $final_array['result'] = [];
+            $final_array['count'] = [];
+            dd(json_decode(json_encode($final_array)));
+        }
+        
+        $apiReqData = json_decode($request->get('req'));
+        
+        
+        // sha1 40bit
+        // md5 32bit
+        
+//        dd(sha1('!@#AEDRFHV %234!23'));
+//        dd(md5('s'));
+        
+//        dd(env('API_TOKEN'));
+        
+        if($apiReqData->api_token != env('API_TOKEN')){
+            $final_array['message'] = "Api token mismatch";
+            $final_array['status'] = 400;
+            $final_array['result'] = [];
+            $final_array['count'] = [];
+            dd(json_decode(json_encode($final_array)));
+        }
+        
+        if($apiReqData->api_key != env('API_KEY')){
+            $final_array['message'] = "Api Key mismatch";
+            $final_array['status'] = 400;
+            $final_array['result'] = [];
+            $final_array['count'] = [];
+            dd(json_decode(json_encode($final_array)));
+            
+        }
+
+        
+        $model_lists = config('viewmodels');
+        
+        if(empty($model_lists[$apiReqData->model])){
+            $final_array['message'] = "Model name is incorrect";
+            $final_array['status'] = 400;
+            $final_array['result'] = [];
+            $final_array['count'] = [];
+            dd(json_decode(json_encode($final_array)));
+        }
+        
+        $obj_model = $model_lists[$apiReqData->model];
+//        get_class($object)
+        
+//        dd(ucfirst($apiReqData->model));
+        
+        dd(json_decode(json_encode($final_array)));
     }
 }
---
+++ config/viewmodels.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'user' => App\Http\Models\User::class,  
+//  'product' => App\Http\Models\User::class,  
+];
+
+?>
